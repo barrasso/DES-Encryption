89c324e28beead19ff88b76bb32c371bc61ac118
barrasso <mark.barrasso@live.com>
2014-11-24 18:52:48 -0500

    setup form UI

--- Lab7/Form1.Designer.cs
+++ Lab7/Form1.Designer.cs
@@ -28,21 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.fileButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key:";
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(47, 108);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.keyTextBox.TabIndex = 2;
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(47, 53);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(247, 20);
+            this.fileTextBox.TabIndex = 3;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(47, 158);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(155, 158);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            // 
+            // fileButton
+            // 
+            this.fileButton.BackColor = System.Drawing.Color.LightGray;
+            this.fileButton.BackgroundImage = global::Lab7.Properties.Resources.OpenPH;
+            this.fileButton.Location = new System.Drawing.Point(322, 48);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(34, 28);
+            this.fileButton.TabIndex = 6;
+            this.fileButton.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(535, 280);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(391, 210);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "File Encrypt/Decrypt";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button fileButton;
     }
 }
 
