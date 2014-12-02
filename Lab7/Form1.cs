c017eb5e9a6c7a47568413a52e028deaf2eb2930
barrasso <mark.barrasso@live.com>
2014-12-01 23:14:11 -0500

    encryption/decrytion with error checking implemented

--- Lab7/Form1.cs
+++ Lab7/Form1.cs
@@ -15,12 +15,7 @@ namespace Lab7
 {
     public partial class Form1 : Form
     {
-        // strings
-        public string fileNameString;
-        public string outputFileName;
-        public string keyString;
-
-        // byte array
+        // key
         public byte[] key;
 
         // default constructor
@@ -40,10 +35,10 @@ namespace Lab7
             if (!errorWithEncryption())
             {
                 // set key 
-                this.setKey();
+                this.checkKey();
 
                 // add .des extention
-                string output = string.Concat(fileNameString, ".des");
+                string output = string.Concat(fileTextBox.Text, ".des");
 
                 // check if file exists
                 if (File.Exists(output))
@@ -53,7 +48,7 @@ namespace Lab7
                 }
 
                 // perform encryption
-                encryptFile(fileNameString, output, this.key, this.key);
+                encryptFile(fileTextBox.Text, output, this.key, this.key);
             }
         }
 
@@ -62,7 +57,31 @@ namespace Lab7
             // error check
             if (!errorWithDecryption())
             {
-                // decrypt, prompt for overwrites
+                // check key
+                this.checkKey();
+
+                // check if valid des extension
+                if (fileTextBox.Text.EndsWith(".des"))
+                {
+                    // remove .des extension
+                    string output = fileTextBox.Text.Substring(0, fileTextBox.Text.Length - 3);
+
+                    // check if file exists
+                    if (File.Exists(output))
+                    {
+                        // prompt for overwrite
+                        if (MessageBox.Show("Output file exists. Overwrite?", "File Exists", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes) return;
+                    }
+
+                    // perform decryption
+                    decryptFile(fileTextBox.Text, output, this.key, this.key);
+                }
+                
+                // else its not a des file
+                else
+                {
+                    MessageBox.Show("Not a .des file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
             }
         }
 
@@ -75,8 +94,7 @@ namespace Lab7
             if (openDialog.ShowDialog() == DialogResult.OK)
             {
                 // set file text box as file name
-                fileNameString = openDialog.FileName;
-                fileTextBox.Text = fileNameString;
+                this.fileTextBox.Text = openDialog.FileName;
             }
         }
 
@@ -113,7 +131,8 @@ namespace Lab7
 
             // instantiate DES provider
             DES des = new DESCryptoServiceProvider();
-            CryptoStream encStream = new CryptoStream(outStream, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);                
+            CryptoStream encStream = new CryptoStream(outStream, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+            Console.WriteLine("Encrypting...");     
 
             // read input file and write to output using provided key
             while (var < len)
@@ -130,19 +149,83 @@ namespace Lab7
             Console.WriteLine("Encryption Success. Output: {0}",fileNameOut);     
         }
 
-        private void decryptFile(string fileNameIn, string fileNameOut, string secretKey)
+        private void decryptFile(string fileNameIn, string fileNameOut, byte[] desKey, byte[] desIV)
         {
+            // must init file streams
+            FileStream inStream = null;
+            FileStream outStream = null;
 
+            try
+            {
+                // init file streams to handle input and output
+                inStream = new FileStream(fileNameIn, FileMode.Open, FileAccess.Read);
+                outStream = new FileStream(fileNameOut, FileMode.OpenOrCreate, FileAccess.Write);
+                outStream.SetLength((long)0);
+            }
+
+            catch
+            {
+                MessageBox.Show("Could not open source or destination file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+                if (inStream != null)
+                    inStream.Close();
+                if (outStream != null)
+                    outStream.Close();
+
+                return;
+            }
+
+            // init byte array
+            byte[] byteArray = new byte[100];
+            long var = (long)0;
+            long len = inStream.Length;
+
+            // instantiate crypto provider
+            DES des = new DESCryptoServiceProvider();
+
+            // check key
+            KeySizes[] legalKeySizes = des.LegalKeySizes;
+            int blockSize = des.BlockSize;
+
+            CryptoStream encStream = new CryptoStream(outStream, des.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
+            Console.WriteLine("Decrypting...");     
+
+            // flag to check if decryption failed
+            bool decryptFail = false;
+            try
+            {
+                // read input file and write to output using provided key
+                while (var < len)
+                {
+                    int dummy = inStream.Read(byteArray, 0, 16);
+                    encStream.Write(byteArray, 0, dummy);
+                    var = var + (long)dummy;
+                }
+                encStream.Close();
+                Console.WriteLine("Decryption Success. Output: {0}", fileNameOut);     
+            }
+            catch
+            {
+                MessageBox.Show("Bad key or file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                decryptFail = true;
+            }
+
+            // close streams
+            outStream.Close();
+            inStream.Close();
+
+            // if decryption failed, delete old file
+            if (decryptFail) File.Delete(fileNameOut);
         }
 
-        private void setKey()
+        private void checkKey()
         {
             // init key
             this.key = new byte[8];
             int dummy = 0;
-            for (int i = 0; i < keyString.Length; i++)
+            for (int i = 0; i < keyTextBox.Text.Length; i++)
             {
-                this.key[dummy] = (byte)(this.key[dummy] + (byte)this.keyString[i]);
+                this.key[dummy] = (byte)(this.key[dummy] + (byte)this.keyTextBox.Text[i]);
                 dummy = (dummy + 1) % 8;
             }
         }
@@ -151,16 +234,14 @@ namespace Lab7
         private bool errorWithEncryption()
         {
             // check key text box
-            keyString = keyTextBox.Text.ToString();
-            if (keyString.Length == 0)
+            if (this.keyTextBox.Text == "")
             {
                 MessageBox.Show("Please enter a key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return true;
             }
 
             // check for valid file
-            fileNameString = fileTextBox.Text.ToString();
-            if (fileNameString.Length == 0)
+            if (fileTextBox.Text == "")
             {
                 MessageBox.Show("Could not open source or destination file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return true;
@@ -173,15 +254,18 @@ namespace Lab7
         private bool errorWithDecryption()
         {
             // check key text box
-            keyString = keyTextBox.Text.ToString();
-            if (keyString.Length == 0)
+            if (this.keyTextBox.Text == "")
             {
                 MessageBox.Show("Please enter a key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return true;
             }
 
             // check for valid DES file OR empty file text box
-
+            if (fileTextBox.Text == "")
+            {
+                MessageBox.Show("Not a .des file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
 
             else
                 return false;
